f9940054ae4b8a6f8ba1bade1bdcbceb83bea8c3
Bezlonir <michael_bragg99@hotmail.com>
2018-04-16 15:21:54 -0600

    Reconfigure transitions.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.site-preview').hover(function(){
-    $('.preview-background').css("display", "block");
-  });
+  // $('.site-preview').hover(function(){
+  //   $('.preview-background').css("display", "block");
+  // });
 });
